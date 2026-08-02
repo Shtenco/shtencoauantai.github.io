7f2229d881188aba690e89f497dfc772e9d63b38
Evgeniy Koshtenko <144296504+Shtenco@users.noreply.github.com>
2026-08-02 11:10:07 +0500

    Add exact solc Polygon fork config for Bullish QuickSwap V2

---
+++ bullish_cb_v2_ci/hardhat.config.js
@@ -0,0 +1,28 @@
+const { subtask } = require("hardhat/config");
+const { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } = require("hardhat/builtin-tasks/task-names");
+require("@nomicfoundation/hardhat-ethers");
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD).setAction(async ({ solcVersion }) => ({
+  compilerPath: require.resolve("solc/soljson.js"),
+  isSolcJs: true,
+  version: solcVersion,
+  longVersion: require("solc").version(),
+}));
+
+module.exports = {
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      optimizer: { enabled: true, runs: 500 },
+      viaIR: true,
+      evmVersion: "cancun"
+    }
+  },
+  networks: {
+    polygon: {
+      chainId: 137,
+      url: process.env.LOCAL_FORK_URL || "http://127.0.0.1:8545"
+    }
+  },
+  mocha: { timeout: 600000 }
+};
